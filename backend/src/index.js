ba7227530eb740a41551142c917da88ae006bacf
pantherofficial404 <pantherofficial404@gmail.com>
2019-09-20 00:37:10 +0530

    Deployment config added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
 const app = require('express')();
+const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 const Debug = require('debug');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const absPath = require('app-module-path');
+const path = require('path');
 
 absPath.addPath(__dirname);
 const router = require('Routes');
+const { portalServe } = require('Middleware');
 
 const debug = Debug('app:server');
 const error = Debug('app:error');
@@ -21,9 +24,13 @@ const server = (async () => {
     });
     debug('Connected to database');
     app.use(cors());
+    app.use(express.static(path.join(__dirname, '../../portal/build/index.html')));
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     app.use('/api/1.0', router);
+    if (process.env.NODE_ENV === 'production') {
+      app.use(portalServe);
+    }
     app.listen(config.get('port'));
     debug('Server is running...');
   } catch (err) {
